2f7a4f2d0b49e56ccf3513d8ed996576daaf951d
kalebe misael <kalebemisael@gmail.com>
2023-07-03 22:15:15 -0300

    :construction: navigator bar

--- ChefDelivery/App/ContentView.swift
+++ ChefDelivery/App/ContentView.swift
@@ -9,18 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            Text("aaaa")
+            Text("aaaa")
         }
-        .padding()
+       
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.sizeThatFits)
     }
 }
---
+++ ChefDelivery/NavigationBar/NavigationBar.swift
@@ -0,0 +1,39 @@
+//
+//  NavigationBar.swift
+//  ChefDelivery
+//
+//  Created by Mac on 03/07/23.
+//
+
+import SwiftUI
+
+struct NavigationBar: View {
+    var body: some View {
+        HStack{
+            Spacer()
+            
+            Button("R. Japa Baiano, 123") {
+                
+            }
+            .font(.subheadline)
+            .fontWeight(.semibold)
+            .foregroundColor(.black)
+            
+            Spacer()
+            
+            Button(action: {}){
+                Image(systemName: "bell.badge")
+                    .font(.title3)
+                    .foregroundColor(.red)
+            }
+        }
+    }
+}
+
+struct NavigationBar_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationBar()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
