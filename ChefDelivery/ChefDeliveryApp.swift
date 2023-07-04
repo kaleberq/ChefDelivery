86e4e4ad4ce784da14c49f8169d1dcdad34529f7
kalebe misael <kalebemisael@gmail.com>
2023-07-03 21:26:53 -0300

    first commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Mac on 03/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
