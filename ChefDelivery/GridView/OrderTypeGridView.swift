3c66bae40d8b5a766f0903502758d66d627627d4
kalebe misael <kalebemisael@gmail.com>
2023-07-04 22:40:26 -0300

    :construction: LazyGrid

---
+++ ChefDelivery/GridView/OrderTypeGridView.swift
@@ -0,0 +1,20 @@
+//
+//  OrderTypeGridView.swift
+//  ChefDelivery
+//
+//  Created by Mac on 04/07/23.
+//
+
+import SwiftUI
+
+struct OrderTypeGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OrderTypeGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderTypeGridView()
+    }
+}
---
+++ ChefDelivery/Model/DataSourceMock.swift
@@ -0,0 +1,8 @@
+//
+//  DataSourceMock.swift
+//  ChefDelivery
+//
+//  Created by Mac on 04/07/23.
+//
+
+import Foundation
